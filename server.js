68a355ea8b93e50ad9caabd63c748d2d52fa30bf
Nick <ndasto@protonmail.com>
2020-07-15 19:24:41 -0400

    heroku three

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require("path");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -16,8 +17,12 @@ app.use("/api/v1/brews", brews);
 
 const PORT = process.env.PORT;
 
-if (process.env.NODE_ENV === "production")
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(
   PORT,
