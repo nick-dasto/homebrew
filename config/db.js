fb28c732a1c9541904aa1b32c89514962cf1a0ab
Nick <ndasto@protonmail.com>
2020-07-16 11:02:20 -0400

    heroku......

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGODB_URI);
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
